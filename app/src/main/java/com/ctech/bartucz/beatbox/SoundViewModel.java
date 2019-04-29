5f8fda1a919aa18592924c0c7f75d6fc0a900ee0
Mr. Bartucz <jobartucz@isd535.org>
2019-04-29 12:19:22 -0500

    Added the SoundViewModel basic class

---
+++ app/src/main/java/com/ctech/bartucz/beatbox/SoundViewModel.java
@@ -0,0 +1,24 @@
+package com.ctech.bartucz.beatbox;
+
+public class SoundViewModel {
+
+    private Sound mSound;
+    private BeatBox mBeatBox;
+
+    public SoundViewModel(BeatBox beatBox) {
+        mBeatBox = beatBox;
+    }
+
+    public String getTitle() {
+        return mSound.getName();
+    }
+
+    public Sound getSound() {
+        return mSound;
+    }
+
+    public void setSound(Sound sound) {
+        mSound = sound;
+    }
+
+}
